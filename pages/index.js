a452759425b603433ba3b4b7e9cb49c598470556
Ashutosh <ashutoshnjha@gmail.com>
2023-10-29 14:02:21 +0530

    Links for other pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
-
+import Link from 'next/link'
 export default class extends Component {
   render () {
     return (
-      <div>pages/index.js</div>
+      <div>
+        pages/index.js
+        <ul>
+          <li>
+            <Link href="/">Root</Link>
+          </li>
+          <li>
+            <Link href="/uitest">UI-TEST</Link>
+          </li>
+          <li>
+            <Link href="/info">Info</Link>
+          </li>
+          <li>
+            <Link href="/service/hello-world">service Post</Link>
+          </li>
+        </ul>
+      </div>
+      
     )
   }
 }
